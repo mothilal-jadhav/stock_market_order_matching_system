0df61fff572bfcc6e4e5babddeff6821aedadb1a
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-03-06 21:25:24 +0530

    trade simulation code

---
+++ Simulator/trade_simulation.py
@@ -0,0 +1,42 @@
+import time
+import random
+
+from engine.matching_engine import MatchingEngine
+from engine.order import order, orderSide
+
+class TradeSimulator:
+    def __init__(self, engine: MatchingEngine):
+        self.engine = engine
+
+    def generate_random_order(self):
+
+        side = random.choice([orderSide.BUY, orderSide.SELL])
+
+        price = round(random.uniform(95, 105), 2)
+
+        quantity = random.randint(1, 100)
+
+        return Order(
+            side=side,
+            price=price,
+            quantity=quantity
+        )
+    
+    def run_simulation(self, num_orders=1000):
+
+        start_time = time.time()
+
+        for _ in range(num_orders):
+
+            order = self.generate_random_order()
+
+            self.engine.submit_order(order)
+
+        end_time = time.time()
+
+        duration = end_time - start_time
+
+        print("Simulation complete")
+        print("Orders processed:", num_orders)
+        print("Trades executed:", len(self.engine.trade_history))
+        print("Time taken:", duration, "seconds")
